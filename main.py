4b48ae37f80014b7c2c621106605b4c9ebf6b79f
edcilo <edcilop@gmail.com>
2021-10-04 22:21:08 -0500

    new lyn route

--- main.py
+++ main.py
@@ -10,5 +10,9 @@ def index():
 def hello():
     return "hello hello kitty"
 
+@app.route('/lyn')
+def lyn():
+    return "Hi, my name is lyn"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
